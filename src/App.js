490350ae0b771ae44186bf9fe16ce02703dfa295
Jake Toomay <jake@toomay.dev>
2023-02-26 14:04:26 -0600

    Conected to host

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { Outlet } from 'react-router-dom';
 import tw, { styled } from 'twin.macro';
-import { useState } from 'react';
 
 function App() {
 
-  const [toggle, setToggle] = useState(false);
-
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <Wrapper>
+      <NavBar>
+        <Title>JakeGPT</Title>
+      </NavBar>
       
-      </header>
-    </div>
-  );
+      <Content>
+        <Outlet />
+      </Content>
+    </Wrapper>
+  )
 }
 
 export default App;
 
-
+const Wrapper = tw.div`fixed w-screen h-screen bg-zinc-800`
+const Title = tw.h1`text-4xl text-white`
+const NavBar = tw.nav`flex items-center  px-10 py-5 bg-zinc-900`
+const Content = tw.div`mx-auto max-w-7xl mt-4 text-white`
---
+++ src/Pages/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Loading from '../Components/Loading'
+
+
+export default function Home() {
+  return (
+        <div>
+            
+        </div>
+  )
+}
